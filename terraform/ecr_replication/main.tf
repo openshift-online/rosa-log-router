3e3718a579300c53d71c53e793cc1090945b8bfb
Feng Huang <fehuang@redhat.com>
2025-09-17 20:20:52 -0400

    add provider for ecr_replication
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- terraform/ecr_replication/main.tf
+++ terraform/ecr_replication/main.tf
@@ -33,6 +33,11 @@ terraform {
   }
   backend "s3" {}
 }
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
 
 resource "aws_ecr_replication_configuration" "ecr_replication" {
   replication_configuration {
